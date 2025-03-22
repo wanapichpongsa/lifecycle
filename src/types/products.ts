70b3e0f79f281cc13d4eab5defcdcefa847b58df
Wana Pichpongsa <wana@pichpongsa.com>
2025-03-22 21:35:06 +0000

    remove data

--- src/types/products.ts
+++ src/types/products.ts
@@ -1,6 +1,4 @@
 export const products = [
-  ["bleh","bleh","ClockoWe","2025-03-25","5 years left","1742679054983-508850832-m2.jpg"],
-  ["SN:HTCCL0032024H10","Large Projector","ClockoWe","2025-03-25","5 years left","1742679024188-448751936-projector.jpg"],
   ["00000001", "iPhone 11", "Apple", "2020-01-01", "1 month left", "iphone.jpg"],
   ["00000002", "Macbook M2", "Apple", "2024-01-01", "4 years 10 months left", "m2.jpg"],
   ["00000003", "NVIDIA Blackwell GPU", "NVIDIA", "2025-01-01", "4 years left", "None"],
